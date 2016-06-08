28c5e992899c1b2dc942438468f03179e122a0f7
David Atias <davidalejandro23@gmail.com>
2016-06-08 17:31:34 -0400

    Refact: nombres legibles

--- bienEtiquetado.pl
+++ bienEtiquetado.pl
@@ -1,7 +1,12 @@
-bienEtiquetado(nodo(Et_Nodo,Aristas)) :- bienEtiquetado_aux(Et_Nodo,Aristas).
-bienEtiquetado_aux(_,[]).
-bienEtiquetado_aux(Et_Nodo1,[Arista | L_Aristas]) :- bienEtiquetado_aux(Et_Nodo1,Arista), bienEtiquetado_aux(Et_Nodo1,L_Aristas).
-bienEtiquetado_aux(Et_Nodo1,arista(Et_Arista,nodo(Et_Nodo2,Aristas2))) :-
+bienEtiquetado(nodo(Et_Nodo,Aristas)) :- bienEtiquetado_aux([],[],Et_Nodo,Aristas).
+
+bienEtiquetado_aux(_,_,_,[]).
+
+bienEtiquetado_aux(Set_Et_Nodos, Set_Et_Aristas, Et_Nodo1, [Arista | L_Aristas]) :-
+	bienEtiquetado_aux(Set_Et_Nodos, Set_Et_Aristas, Et_Nodo1, Arista),
+	bienEtiquetado_aux(Set_Et_Nodos, Set_Et_Aristas, Et_Nodo1, L_Aristas).
+
+bienEtiquetado_aux(Set_Et_Nodos, Set_Et_Aristas, Et_Nodo1, arista(Et_Arista,nodo(Et_Nodo2,Aristas2))) :-
 	Dif is Et_Nodo1-Et_Nodo2,
 	abs(Dif, Dif_Abs),
 	Dif_Abs == Et_Arista,
