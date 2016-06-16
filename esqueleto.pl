1bf47ceaf1ec7897253e1095a0ab1be0d94cd5d6
David Atias <davidalejandro23@gmail.com>
2016-06-16 02:46:48 -0400

    Buenos etiq, no todos, pero algunos

--- esqueleto.pl
+++ esqueleto.pl
@@ -38,18 +38,79 @@ constrLista(N,R,Min,Restantes,Tam,[Cabeza | L]) :-
 slice([],_,_,[]) :- !.
 slice([Cabeza | Resto], 0, 0, []) :- !.
 slice([Cabeza | Resto], 0, Tam, [Cabeza | L]):- T is Tam - 1, slice(Resto,0,T,L). 
-slice([Cabeza | Resto], Inicio, Tam, L) :- I is Inicio-1, T is Tam-1,slice(Resto, I, T, L).
+slice([Cabeza | Resto], Inicio, Tam, L) :- I is Inicio-1, T is Tam-1, slice(Resto, I, T, L).
 
-etiquetamiento([],_,_,[]).
-etiquetamiento([[] | _],_,_,[]).
-etiquetamiento([[] | Resto],Inicio,Tam,[]).
-etiquetamiento([[0 | Cola] | Resto],Inicio,Tam,[arista(1,nodo(1,[])) | LAristas]) :- ISig is Inicio + 1, etiquetamiento([Cola | Resto],ISig,Tam,LAristas).
-etiquetamiento([[Cab] | Lista], nodo(1,L)) :- etiquetamiento(Lista, 0, Cab, L).
-etiquetamiento([[Cabeza | [CabSig | Cola]] | [LAristas | Resto]], Inicio, Tam, [arista(1,nodo(1,Arista)) | LAristas]) :-
-	slice(LAristas, Inicio, Cabeza, MisAristas),
-	etiquetamiento([MisAristas | Resto],Inicio, Cabeza, Arista),
+head([X | _],X).
+
+etiquetamiento([[]],_,_,[]).
+etiquetamiento([[Cab] | Lista], Arbol) :- etiquetamiento(Lista, 0, Cab, L), etiquetar(nodo(EtNodo,L),Arbol).
+
+genAristasHojas(0,[]).
+genAristasHojas(Tam,[arista(X,nodo(Y,[])) | Resto]) :- T is Tam - 1, genAristasHojas(T, Resto).
+etiquetamiento([[0 | Resto]],Inicio,Tam,Aristas) :- genAristasHojas(Tam,Aristas), !.
+
+etiquetamiento([[Cabeza | Cola] | [LAristas | Resto]], Inicio, Tam, [arista(EtArista,nodo(EtNodo,Arista)) | RestoAristas]) :-
 	ISig is Inicio + Cabeza,
-	etiquetamiento([Cola | [LAristas | Resto]],ISig,CabSig,LAristas).
+	slice(LAristas, Inicio, ISig, MisAristas),
+	etiquetamiento([MisAristas | Resto],Inicio, Cabeza, Arista),
+	((head(Cola,CabSig),
+		etiquetamiento([Cola | [LAristas | Resto]],ISig,CabSig,RestoAristas)
+	); RestoAristas = [], !).
+
+% Caso base: Si un nodo no tiene subarboles, el tamaño de sus subarboles es 0
+tamArbol([],0).
+
+% El tamaño de un arbol es 1 + el tamaño de sus subarboles hijos
+tamArbol(nodo(_, Aristas),N) :- tamArbol(Aristas,_N), N is _N+1.
+
+% El tamaño de una lista de subarboles hijos es el tamaño del primer subarbol más
+% el tamaño del resto de los subarboles en la lista.
+tamArbol([ arista(_,Sub_Arbol) | Aristas2],N) :-
+	tamArbol(Sub_Arbol,N1),
+	tamArbol(Aristas2,N2), N is N1+N2.
+	
+generarL(0, []) :- !.
+generarL(N, [N | Lista]):- Nsig is N-1, generarL(Nsig,Lista).
+	
+etiquetar(Arbol, ArbolN):-
+    tamArbol(Arbol, N), 
+    generarL(N, ListaN),
+    delete(ListaN, N,ListaA),
+    etiquetar(Arbol, ListaN,ListaA, ArbolN).
+
+etiquetar([],_,ListaN,ListaA,ListaN,ListaA,[]).
+
+etiquetar(nodo(EtNodo, [arista(EtArista,nodo(EtNodo2,Aristas2)) | Aristas]), ListaN,ListaA, nodo(EtNodo, [arista(EtArista,nodo(EtNodo2,Aristas2)) | Aristas])) :-
+    tamArbol(nodo(EtNodo, [arista(EtArista,nodo(EtNodo2,Aristas2)) | Aristas]), N),
+    between(1,N,EtNodo),
+    ESig is EtNodo + 1,
+    between(ESig,N,EtNodo2),
+    EtArista is abs(EtNodo-EtNodo2),
+    (
+    	(
+	    	member(EtArista,ListaA),
+		    subtract(ListaN,[EtNodo,EtNodo2],ListaNSig),
+		    delete(ListaA,EtArista,ListaASig),
+		    etiquetar(Aristas2, EtNodo2, ListaNSig, ListaASig, ListaNR, ListaAR, Aristas2),
+		    etiquetar(Aristas, EtNodo, ListaNR, ListaAR, ListaADef, ListaNDef, Aristas)
+		); (fail, !)
+	).
+    
+etiquetar([arista(EtArista,nodo(EtNodo,Aristas)) | Resto], EtPadre, ListaN, ListaA, ListaNDef, ListaADef, [arista(EtArista,nodo(EtNodo,Aristas)) | Resto]) :-
+    length(ListaN,TamLN),
+    between(1,TamLN,I),
+    nth1(I,ListaN,EtNodo),
+    EtArista is abs(EtPadre-EtNodo),
+    (
+    	(
+	    	member(EtArista,ListaA),
+		    delete(ListaN,EtNodo,ListaNSig),
+		    delete(ListaA,EtArista,ListaASig),
+		    etiquetar(Aristas, EtNodo, ListaNSig, ListaASig, ListaNR, ListaAR, Aristas),
+		    etiquetar(Resto, EtPadre, ListaNR, ListaAR, ListaNDef, ListaADef, Resto)
+		); (fail, !)
+	).
+
 
 describirEtiquetamiento(nodo(E,Aristas)) :- 
 	write(0), write(": "), write(E), nl,
